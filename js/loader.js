66100c878f18c6588772f6ba8dddac9a80bcc5d9
Timoti11 <timastanskov@mail.ru>
2022-05-11 22:50:26 +0300

    Add files via upload
    - сделал раздел покупки
    - сделал плашки платно и бесплатно
    - оптимизировал цвета через переменнный colors для удобства
    - копирование айпи по нажатию
    - вспл. уведомление о копирке

---
+++ js/loader.js
@@ -0,0 +1,5 @@
+$(function() {
+ 
+  $(".loader_gif").delay(700).fadeOut();
+  $(".loader").delay(1000).fadeOut("slow");
+});
--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,45 @@
- var menu = $("#header");
-  $(window).scroll(function() {
-    var top = $(this).scrollTop();
-    if ( top >= 100 ) {
-      menu.css(background-color: 'green');
-    } else if ( top <= 200 ) {
-      menu.css(background-color: 'blue');
-    }
-  });
+let clipboard = new ClipboardJS('.welcome__button');
+ 
+
+clipboard.on('success', function(e) {
+  console.info('Action:', e.action);
+  console.info('Text:', e.text);
+  console.info('Trigger:', e.trigger);
+});
+
+
+
+s = 1
+$(".welcome__button").click(function(){
+	console.log("s14 = " + s)
+	if (s == 1) {
+		console.log("s15 = " + s)
+		s = 0;
+		console.log("s17 = " + s)
+		firstStep();
+		console.log("s19 = " + s)
+		setTimeout('secondStep()',1000);
+		console.log("s21 = " + s)
+		setTimeout('s = 1;',1500);
+		console.log("s23 = " + s)
+	} else {
+	}
+}); 
+
+function firstStep(){
+		$("#copied").animate(
+			{
+    			opacity: 1,
+    			top: "+=50",
+  			}, 500
+  		);
+}
+function secondStep(){
+	$("#copied").animate(
+			{
+		    	opacity: 0,
+		    	top: "-=50",
+		    	height: "toggle",
+		  	}, 500
+		);
+}
